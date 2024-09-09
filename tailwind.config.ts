c45b7a232f1b2a33a722d6e3e2932610c82d2338
Reload0213 <dldudwn7227@gmail.com>
2024-09-10 02:18:46 +0900

    Feat: init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,15 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import withMT from '@material-tailwind/react/utils/withMT'; // material-tailwind 사용 조건
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+    content: [
+        './utils/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {},
+    plugins: [require('@tailwindcss/typography')],
 };
-export default config;
+
+export default withMT(config); // material-tailwind 사용 조건
